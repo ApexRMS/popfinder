24163f425f61e9fe74b3be853084b8748359abc3
Katie Birchard <33553358+katieb1@users.noreply.github.com>
2022-12-23 15:38:58 -0800

    Working on preprocessing functions

---
+++ popfinder/classifier.py
@@ -0,0 +1,18 @@
+class PopClassifier(object):
+    """
+    A class to represent a classifier neural network object for population assignment.
+    """
+    def __init__(self, random_state):
+        self.random_state = random_state
+
+    def hypertune():
+        pass
+
+    def train():
+        pass
+
+    def test():
+        pass
+
+    def assign_unknown(self):
+        pass
--- popfinder/preprocess.py
+++ popfinder/preprocess.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
-import allel
+import allel # change to sgkit eventually
 import zarr
 import h5py
 import sys
 import os
 
-def read_data(infile, sample_data, save_allele_counts=False):
+from sklearn.model_selection import RepeatedStratifiedKFold, train_test_split
+
+
+def read_data(genetic_data, sample_data):
     """
     Reads a .zarr, .vcf, or h5py file containing genetic data and
     compiles information into a pandas DataFrame for either a 
@@ -14,62 +17,177 @@ def read_data(infile, sample_data, save_allele_counts=False):
 
     Parameters
     ----------
-    infile : string
+    genetic_data : string
         Path to the .zarr, .vcf, or h5py file.
     sample_data : string
         Path to .txt file containing sample information
         (columns are x, y, sampleID, and pop). See documentation
         on help formatting this file.
-    save_allele_counts : boolean
-        Saves derived allele count information (Default=False).
 
     Returns
     -------
-    sample_df : Pandas DataFrame
+    data : Pandas DataFrame
         Contains information on corresponding sampleID and
-        population classifications.
-    dc : np.array
-        Array of derived allele counts.
-    unknowns : dataframe
-        If kfcv is set to False, returns a dataframe with
-        information about sampleID and indices for samples
-        of unknown origin.
+        genetic information.
     """
 
     # Check formats of datatypes
-    if os.path.exists(infile) is False:
-        raise ValueError("Path to infile does not exist")
+    if os.path.exists(genetic_data) is False:
+        raise ValueError("Path to genetic_data does not exist")
 
     if os.path.exists(sample_data) is False:
         raise ValueError("Path to sample_data does not exist")
 
     # Load genotypes
     print("loading genotypes")
-    if infile.endswith(".zarr"):
+    samples, dc = _load_genotypes(genetic_data)
 
-        callset = zarr.open_group(infile, mode="r")
+    # Load data and organize for output
+    print("loading sample data")
+
+    locs = pd.read_csv(sample_data, sep="\t")
+    locs = _sort_samples(locs, samples)
+    locs["alleles"] = list(dc)
+
+    return locs
+
+def split_unknowns(data):
+    """
+    Splits data into known and unknown samples.
+    
+    Parameters
+    ----------
+    data : Pandas DataFrame
+        Contains information on corresponding sampleID and
+        genetic information. This is the output of `read_data()`.
+    
+    Returns
+    -------
+    known : Pandas DataFrame
+        Contains information on corresponding sampleID and
+        genetic information for known samples.
+    unknown : Pandas DataFrame
+        Contains information on corresponding sampleID and
+        genetic information for unknown samples.
+    """
+    # Split data into known and unknown
+    unknown = data[data["pop"].isnull()]
+    known = data[data["pop"].notnull()]
+
+    return known, unknown
+
+def split_train_test(data, stratify_by_pop=True, test_size=0.2, valid_size=None, seed=123):
+    """
+    Splits data into training and testing sets.
+    
+    Parameters
+    ----------  
+    data : Pandas DataFrame
+        Contains information on corresponding sampleID and
+        genetic information. This is the known output of `read_data()`
+        and `split_unknown()`.
+    stratify_by_pop : bool
+        Whether to stratify the data by population. Default is True.
+    test_size : float
+        Proportion of data to be used for testing. Default is 0.2.
+    valid_size : float
+        Proportion of data to be used for validation. Default is None.
+    seed : int
+        Random seed for reproducibility. Default is 123.
+        
+    Returns
+    -------
+    train : Pandas DataFrame
+        Contains information on corresponding sampleID and
+        genetic information for training samples.
+    test : Pandas DataFrame
+        Contains information on corresponding sampleID and
+        genetic information for testing samples.
+    valid : Pandas DataFrame
+        Contains information on corresponding sampleID and
+        genetic information for validation samples. This is only
+        returned if `valid_size` is not None.
+    """
+    # Split data into training and testing
+    if stratify_by_pop is True:
+        train, test = _stratified_split(data, test_size, seed)
+    else:
+        train, test = _random_split(data, test_size)
+
+    if valid_size is not None:
+        # Figure out valid_size based on train set
+        prop = len(data) * valid_size
+        new_valid_size = prop / len(train)
+
+        if stratify_by_pop is True:
+            train, valid = _stratified_split(data, new_valid_size, seed)
+        else:
+            train, valid = _random_split(data, new_valid_size, seed)
+
+        return train, test, valid
+
+    return train, test
+
+def split_kfcv(data, n_splits=5, n_reps=1, stratify_by_pop=True, seed=123):
+    """
+    Splits data into training and testing sets.
+
+    Parameters
+    ----------
+    data : Pandas DataFrame
+        Contains information on corresponding sampleID and
+        genetic information. This is the known output of `read_data()`
+        and `split_unknown()`.
+    n_splits : int
+        Number of splits for cross-validation. Default is 5.
+    n_reps : int
+        Number of repetitions for cross-validation. Default is 1.
+    stratify_by_pop : bool
+        Whether to stratify the data by population. Default is True.
+    seed : int
+        Seed for random number generator. Default is 123.
+
+    Returns
+    -------
+    list of tuples
+        Each tuple contains a training and testing set. The length
+        of the list is n_splits * n_reps.
+    """
+    rskf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_reps, random_state=seed)
+    dataset_list = []
+
+    for _, (train_ind, test_ind) in enumerate(rskf.split(data[:-1], data[-1], groups=data["pop"])):
+        train = data.iloc[train_ind]
+        test = data.iloc[test_ind]
+        dataset_tuple = (train, test)
+        dataset_list.append(dataset_tuple)
+
+    return dataset_tuple
+
+def _load_genotypes(genetic_data):
+
+    if genetic_data.endswith(".zarr"):
+        callset = zarr.open_group(genetic_data, mode="r")
         gt = callset["calldata/GT"]
         gen = allel.GenotypeArray(gt[:])
         samples = callset["samples"][:]
 
-    elif infile.endswith(".vcf") or infile.endswith(".vcf.gz"):
-
-        vcf = allel.read_vcf(infile, log=sys.stderr)
+    elif genetic_data.endswith(".vcf") or genetic_data.endswith(".vcf.gz"):
+        vcf = allel.read_vcf(genetic_data, log=sys.stderr)
         gen = allel.GenotypeArray(vcf["calldata/GT"])
         samples = vcf["samples"]
 
-    elif infile.endswith(".locator.hdf5"):
-
-        h5 = h5py.File(infile, "r")
+    elif genetic_data.endswith(".locator.hdf5"):
+        h5 = h5py.File(genetic_data, "r")
         dc = np.array(h5["derived_counts"])
         samples = np.array(h5["samples"])
         h5.close()
 
     else:
-        raise ValueError("Infile must have extension 'zarr', 'vcf', or 'hdf5'")
+        raise ValueError("genetic_data must have extension 'zarr', 'vcf', or 'hdf5'")
 
     # count derived alleles for biallelic sites
-    if infile.endswith(".locator.hdf5") is False:
+    if genetic_data.endswith(".locator.hdf5") is False:
 
         print("counting alleles")
         ac = gen.to_allele_counts()
@@ -77,25 +195,13 @@ def read_data(infile, sample_data, save_allele_counts=False):
         dc = np.array(ac[biallel, :, 1], dtype="int_")
         dc = np.transpose(dc)
 
-        if (save_allele_counts and
-                not infile.endswith(".locator.hdf5")):
+    return samples, dc
 
-            print("saving derived counts for reanalysis")
-            outfile = h5py.File(infile + ".locator.hdf5", "w")
-            outfile.create_dataset("derived_counts", data=dc)
-            outfile.create_dataset("samples", data=samples,
-                                   dtype=h5py.string_dtype())
-            outfile.close()
+def _sort_samples(locs, samples):
 
-    # Load data and organize for output
-    print("loading sample data")
-
-    locs = pd.read_csv(sample_data, sep="\t")
-
-    if not pd.Series(["x",
-                      "pop",
-                      "y",
-                      "sampleID"]).isin(locs.columns).all():
+    if not pd.Series([
+        "x", "pop", "y", "sampleID"
+        ]).isin(locs.columns).all():
         raise ValueError("sample_data does not have correct columns")
 
     locs["id"] = locs["sampleID"]
@@ -114,18 +220,26 @@ def read_data(infile, sample_data, save_allele_counts=False):
         raise ValueError(
             "sample ordering failed! Check that sample IDs match VCF.")
 
-    else:
+    return locs
 
-        # Find unknown locations as NAs in the dataset
-        unknowns = locs.iloc[np.where(pd.isnull(locs["pop"]))]
+def _stratified_split(data, test_size, seed):
 
-        # Extract known location information for training
-        samples = samples[np.where(pd.notnull(locs["pop"]))]
-        locs = locs.iloc[np.where(pd.notnull(locs["pop"]))]
-        order = np.array(locs["order"])
-        locs = np.array(locs["pop"])
-        samp_list = pd.DataFrame({"samples": samples,
-                                  "pops": locs,
-                                  "order": order})
+    X_train, X_test, y_train, y_test = train_test_split(
+        data[:-1], data[-1], stratify=data["pops"],
+        random_state=seed, test_size=test_size)
 
-    return samp_list, dc, unknowns
+    train = pd.concat([X_train, y_train], axis=1)
+    test = pd.concat([X_test, y_test], axis=1)
+
+    return train, test
+
+def _random_split(data, test_size, seed):
+
+    X_train, X_test, y_train, y_test = train_test_split(
+    data[:-1], data[-1],
+    random_state=seed, test_size=test_size)
+
+    train = pd.concat([X_train, y_train], axis=1)
+    test = pd.concat([X_test, y_test], axis=1)
+
+    return train, test
